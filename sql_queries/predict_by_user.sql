de5f5f4d4c2f8da9e435383b95b90c6f0d19f97d
Julien Duranton <julien.duranton@onmo.com>
2023-01-31 09:14:27 -0500

    new query

--- sql_queries/predict_by_user.sql
+++ sql_queries/predict_by_user.sql
@@ -1,1 +1,3 @@
-SELECT created_at,app_id,user_id,session_type FROM onmo.game_sessions where user_id like '96e9ce4f-2e93-42fd-b14c-01625c684690' and date(created_at) >= '2022-01-01' and app_id not like 'embed-subwaysurfer';
+SELECT created_at,app_id,user_id,session_type FROM onmo.game_sessions 
+where user_id in ('f3b51e88-594b-4bb9-b732-3f28859f2011','40f10905-96ed-4808-824e-c9b45e373919') 
+and date(created_at) >= '2022-01-01' and app_id not like 'embed-subwaysurfer';
